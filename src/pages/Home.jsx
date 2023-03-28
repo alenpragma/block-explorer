4da37ded2eee61b0d382f5eb62a6f2dd4a544c86
Hamza Altamas <hamza.altamas.01876@gmail.com>
2023-03-28 08:26:05 +0600

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Hofdgsssssssssssssssssssssssssssssssssssssssssssssssssssme</div>;
+  return <div>you</div>;
 };
 
 export default Home;
