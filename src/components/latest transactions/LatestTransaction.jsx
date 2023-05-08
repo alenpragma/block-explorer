acbd21a27f3bb4c6b8fb7358f7ab9541444a640a
Your Name <you@example.com>
2023-05-08 12:42:30 +0600

    upg

--- src/components/latest transactions/LatestTransaction.jsx
+++ src/components/latest transactions/LatestTransaction.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-import "./latestTrans.css";
+//import "./latestTrans.css";
 import { LatestTrans } from "./LatestTransData";
 import { CgFileDocument } from "react-icons/cg";
 
