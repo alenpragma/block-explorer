ee3f7d9a42205a471c33dc613a0a9518f7697c6f
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2023-05-13 18:14:18 +0600

    price-api.js loc st added

--- src/components/homeChartBar.jsx/price-api.js
+++ src/components/homeChartBar.jsx/price-api.js
@@ -1,9 +1,37 @@
 import axios from 'axios';
 
+//const API_KEY = 'coinrankingd047dc230e2b59332b8cc15e37bb477497cc41230fb7cd5c';
+//const API_URL = 'https://api.coinranking.com/v2/coin/htA2C2yTr/price';
+
+//let mindCoinPrice = null;
+
+//async function fetchMindCoinPrice() {
+// try {
+ //   const response = await axios.get(API_URL, {
+  //    headers: {
+   //     'x-access-token': API_KEY
+  //    }
+  //  });
+ //   mindCoinPrice = response.data.data.price;
+ // } catch (error) {
+  //  console.log(error);
+//}
+//}
+
+
+//fetchMindCoinPrice();
+
+
+//setInterval(fetchMindCoinPrice, 10000);
+
+//export { mindCoinPrice };
+
+//import axios from 'axios';
+
 const API_KEY = 'coinrankingd047dc230e2b59332b8cc15e37bb477497cc41230fb7cd5c';
 const API_URL = 'https://api.coinranking.com/v2/coin/htA2C2yTr/price';
 
-let mindCoinPrice = null;
+let mindCoinPrice = localStorage.getItem('mindCoinPrice') || null;
 
 async function fetchMindCoinPrice() {
   try {
@@ -12,16 +40,19 @@ async function fetchMindCoinPrice() {
         'x-access-token': API_KEY
       }
     });
-    mindCoinPrice = response.data.data.price;
+    const newPrice = response.data.data.price;
+    if (newPrice !== mindCoinPrice) {
+      mindCoinPrice = newPrice;
+      localStorage.setItem('mindCoinPrice', mindCoinPrice);
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
-
 fetchMindCoinPrice();
-
-
 setInterval(fetchMindCoinPrice, 10000);
 
 export { mindCoinPrice };
+
+
