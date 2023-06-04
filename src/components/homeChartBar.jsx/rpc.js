d4f337d2e2dfed241dc698c80496c1e6fb3c3f99
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2023-06-04 21:05:13 +0600

    Update rpc.js

--- src/components/homeChartBar.jsx/rpc.js
+++ src/components/homeChartBar.jsx/rpc.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
 
-const provider = new Web3('https://mainnet-rpc.metaviralscan.com');
+const provider = new Web3('https://rpc-msc.mindchain.info/');
 
 export { provider };
